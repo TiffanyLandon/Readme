e6cdf9aba5cb49d35e15b27d9956b272154d2e4b
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-02-27 23:38:59 -0500

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,150 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'Input',
+            name: 'title',
+            Message: 'What is the title of the project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('What is the title of the project?');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmDescription',
+            message: 'Would you like to provide a short description explaining the what, why, and how of your project?.',
+            default: true,
+        },
+        {
+            type: 'Input',
+            name: 'Description',
+            message: 'Provide a short description explaining the what, why, and how of your project.',
+            when: ({ confirmDescription }) => confirmDescription
+        },
+        {
+            type: 'Input',
+            name: 'Installation Usage',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'Input',
+            name: 'usage info',
+            message: 'Provide instructions and examples for use. Include screenshots as needed.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Provide instructions and examples for use. Include screenshots as needed.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'Input',
+            name: 'contributing',
+            message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'Input',
+            name: 'Test',
+            message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Go the extra mile and write tests for your application. Then provide examples on how to run them here.');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license does the project consit of?',
+            choices: ["MIT", "IPL 1.0", "ISC", "MPL 2.0"]
+        },
+        {
+            type: 'Input',
+            name: 'Github Username',
+            message: 'Please provide your github username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Provide your github username');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'Input',
+            name: 'email',
+            message: 'Please provide a email address',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Provide a email address');
+                    return false;
+                }
+            }
+        }
+
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'file created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+init = () => {
+    return questions()
+        .then(userData => {
+            return generateMarkdown(userData)
+        })
+        .then(markdown => {
+            return writeToFile('./dist/readme.md', markdown)
+        })
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+  if (license) {
+    return '![License:${license}](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (license) => {
+  if (license) {
+    return '![License:${license}]Click here for the license link](https://opensource.org/licenses/${license.replace('; ')})'
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (license) => {
+  if (license) {
+    return '![license:${license}]'
+  }
+  return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of content
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test) 
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contribution
+  
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+  ##Questions
+  
+  GitHub Repo](https://github.com/${data.gitHubUserName})
+  
+  email :${data.email}
 `;
 }
 
